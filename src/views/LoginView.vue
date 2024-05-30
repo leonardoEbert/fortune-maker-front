4d577aa25bb7dd66b1a04584b13a9a02f34eb076
Leonardo Ebert <leoebert19@gmail.com>
2024-05-29 22:09:33 -0300

    Initial commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LoginView',
+  data() {
+    return {
+      message: 'Hello World!'
+    }
+  },
+  methods: {
+    buttonClicked() {
+      alert('Hello World!');
+    }
+  }
+})
+</script>
+
+<template>
+  <el-button @click="buttonClicked">{{ message }}</el-button>
+</template>
+
+<style scoped>
+
+</style>
