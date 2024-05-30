c84f0b415f4833b986c5aab2716a1f364c3d3379
Leonardo Ebert <leoebert19@gmail.com>
2024-05-30 17:25:15 -0300

    feat: clean vue default views and themes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="el-row" style="width: 100%">
+    <el-container class="el-col-24">
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-main>Main</el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
 </template>
