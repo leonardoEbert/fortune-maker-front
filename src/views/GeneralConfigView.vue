8494fc8273672d875dc6f0760baec2e28a1f7396
Leonardo Ebert <leoebert19@gmail.com>
2024-06-01 00:35:07 -0300

    feat: config view initial commit

---
+++ src/views/GeneralConfigView.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'GeneralConfigView'
+})
+</script>
+
+<template>
+  <h1>GENERAL CONFIG VIEW</h1>
+</template>
+
+<style scoped>
+
+</style>
