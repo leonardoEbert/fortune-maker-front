d3d831d05da203320783924091c590d4dcfd481f
Leonardo Ebert <leoebert19@gmail.com>
2024-06-02 18:04:44 -0300

    feat: added new service for auth requests

---
+++ src/service/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import AxiosService from '@/common/axios.service'
+import type { LoginDto } from '@/dto/login.dto'
+import { useAuthStore } from '@/stores/auth'
+import type { AuthData } from '@/model/AuthData.model'
+
+export class AuthService {
+  private axiosInstance = AxiosService;
+
+  constructor() {
+    this.axiosInstance = AxiosService;
+  }
+
+  public checkToken(token: string) {
+    console.log(token);
+  }
+
+  public async tryLogin(loginData: LoginDto) {
+    this.axiosInstance.post<AuthData>('/auth/login', loginData)
+      .then((response) => {
+        const authStore = useAuthStore()
+        authStore.setToken(response.accessToken)
+        authStore.setExpiration(response.expiresAt)
+      })
+
+  }
+}
