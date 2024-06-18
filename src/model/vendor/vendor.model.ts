155a64021738e56a4bafa8741444888117876ff5
Leonardo Ebert <leoebert19@gmail.com>
2024-06-17 21:47:21 -0300

    feat: added vendor model

---
+++ src/model/vendor/vendor.model.ts
@@ -0,0 +1,12 @@
+import type { VendorClassification } from '@/model/vendor/vendor-classification.model'
+
+export class Vendor {
+  id: string = '';
+  name: string = '';
+  description: string = '';
+  isActive: boolean = false;
+  createdAt: Date = new Date();
+  updatedAt: Date = new Date();
+  deletedAt?: Date;
+  classifications: VendorClassification[] = [];
+}
