698ded3c04d969120c166ed975997841fd507dc9
Leonardo Ebert <leoebert19@gmail.com>
2024-06-17 21:40:43 -0300

    style: added missing semi

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { useAuthStore } from '@/stores/auth'
-import { AuthService } from '@/service/auth/auth.service'
+import { createRouter, createWebHistory } from 'vue-router';
+import { useAuthStore } from '@/stores/auth';
+import { AuthService } from '@/service/auth/auth.service';
 // Views
-import HomeView from '@/views/HomeView.vue'
-import LoginView from '@/views/LoginView.vue'
-import GeneralConfigView from '@/views/GeneralConfigView.vue'
-import VendorView from '@/views/vendor/VendorView.vue'
-import VendorClassificationView from '@/views/vendor/VendorClassificationView.vue'
+import HomeView from '@/views/HomeView.vue';
+import LoginView from '@/views/LoginView.vue';
+import GeneralConfigView from '@/views/GeneralConfigView.vue';
+import VendorView from '@/views/vendor/VendorView.vue';
+import VendorClassificationView from '@/views/vendor/VendorClassificationView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -40,19 +40,19 @@ const router = createRouter({
       component: LoginView
     }
   ]
-})
+});
 
 router.beforeEach((to) => {
-  const authStore = useAuthStore()
-  if (to.meta.requiresAuth && (authStore.isLoggedIn === 'false')) return '/login'
+  const authStore = useAuthStore();
+  if (to.meta.requiresAuth && (authStore.isLoggedIn === 'false')) return '/login';
 })
 
 router.isReady().then(() => {
   const authService = new AuthService();
-  const authStore = useAuthStore()
+  const authStore = useAuthStore();
 
   if (authStore.token) {
-    authService.checkToken(authStore.token)
+    authService.checkToken(authStore.token);
   }
 })
 
